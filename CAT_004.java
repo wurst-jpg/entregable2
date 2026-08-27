71a9ab5708406385612be77a7b1751a890f9462a
wurst-jpg <wurs784@gmail.com>
2026-08-27 12:18:12 -0500

    agregando funcionalidades al codigo y logica

--- CAT_004.java
+++ CAT_004.java
@@ -5,12 +5,16 @@ public class CAT_004 {
         double DESCUENTO = 0.1;
         double IVA = 0.19;
         int opcion;
+        double porcentajeTotalPremiun;
         int basico, medio, premiun;
+        int totalItemns = 0;
+        int total = 0;
+        int contadorPremiun = 0;
         basico = 50000;
         medio = 100000;
         premiun = 150000;
         Scanner leer = new Scanner(System.in);
-        
+
         do {
             System.out.println("╔══════════════════════════════════╗\n" + //
                                 "║     CATALOGO LOCAL - CAJA #1     ║\n" + //
@@ -22,12 +26,24 @@ public class CAT_004 {
                                 "╚══════════════════════════════════╝");
             System.out.print("  Seleccione una opcion: ");
             opcion = leer.nextInt();
-            System.out.println("no saliste");
-        } while (opcion != 4); 
-        
-        
+            if (opcion == 1) {
+                total += 50000;
+                totalItemns++;
+                System.out.println("producto basico agregado a la compra");
+            } else if (opcion == 2) {
+                total += 100000;
+                System.out.println("producto medio agregado a la compra");
+                totalItemns++;
+            } else if (opcion == 3) {
+                total += 150000;
+                System.out.println("producto premiun agregado a la compra");
+                contadorPremiun++;
+                totalItemns++;
+            }
+        } while (opcion != 4);
+        porcentajeTotalPremiun = (contadorPremiun * 100.0) / totalItemns;
+        System.out.println(porcentajeTotalPremiun);
+        System.out.println(total);
         System.out.println("saliste");
     }
-    
 }
-
