9d66c71292d4f3788713c68f09e0c8ff910f7379
wurst-jpg <wurs784@gmail.com>
2026-08-26 22:38:12 -0500

    creando la carpeta del proyecto y definiendo variables del proyecto

---
+++ CAT_004.java
@@ -0,0 +1,11 @@
+public class CAT_004 {
+    public static void main(String[] args) {
+        double DESCUENTO = 0.1;
+    double IVA = 0.19;
+    int basico, medio, premiun;
+    basico = 50000;
+    medio = 100000;
+    premiun = 150000;
+    
+    }
+}
